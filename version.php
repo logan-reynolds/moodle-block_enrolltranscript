78e678eaed449574bf1a17e8648a0d76b23163c5
Logan Reynolds <logan.reynolds@remote-learner.net>
2016-11-18 16:55:37 -0600

    MAE-2463 - Incremented version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012112900;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2012112901;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component    = 'block_enrolltranscripts';
 $plugin->maturity = MATURITY_STABLE;
